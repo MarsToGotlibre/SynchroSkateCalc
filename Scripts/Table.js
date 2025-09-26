8ef3ee24328650bc1c08211e3ced128be6b2e3d1
MarsToGotlibre <231478721+MarsToGotlibre@users.noreply.github.com>
2025-09-26 13:32:10 +0200

    Program content Table Display

---
+++ Scripts/Table.js
@@ -0,0 +1,44 @@
+import { Program } from "./ScoreCalc.js";
+
+export function RenderName(elem) {
+   let name = ""
+   if (elem.Specification && elem.Specification != "Cr") {
+      name = elem.Specification + elem.Element + elem.Lvl
+
+   } else if (elem.AdditionalFeature) {
+      name = elem.Element + elem.Lvl + " + " + elem.AdditionalFeature + elem.AdditionalFeatureLvl
+   } else {
+      name = elem.Element + elem.Lvl
+   }
+
+   if (elem.Downgrades) {
+      name += elem.Downgrades
+   }
+   return name
+}
+
+export function renderElements() {
+   const tbody = document.querySelector('.displayTable');
+   tbody.innerHTML = '';
+   Program.Elements.forEach((parts, idx) => {
+      const tr = document.createElement('tr');
+      tr.dataset.index = String(idx);
+      tr.draggable = true;
+      tr.innerHTML = `
+          <td class="text-center"><i class="bi bi-grip-vertical handle"></i></td>
+          <td>${idx + 1}</td>
+          <td>${RenderName(parts)}</td>
+          <td>${parts.BaseValue}</td>
+          <td>${parts.GOE > 0 ? ('+' + parts.GOE) : parts.GOE}</td>
+          <td>${parts.GoeValue.toFixed(2)}</td>
+          <td class="elemScore">${parts.ElementScore.toFixed(2)}</td>
+          <td class="text-nowrap">
+            <button class="btn btn-sm btn-outline-secondary edit"><i class="bi bi-pencil"></i></button>
+            <button class="btn btn-sm btn-outline-danger delete"><i class="bi bi-trash"></i></button>
+          </td>`;
+      tbody.appendChild(tr);
+   });
+
+}
+window.renderElements = renderElements
+window.RenderName = RenderName
