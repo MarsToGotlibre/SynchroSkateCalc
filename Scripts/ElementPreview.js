689fde2cc3d7224f6f2a3dcc82025b3c44a79358
MarsToGotlibre <231478721+MarsToGotlibre@users.noreply.github.com>
2025-09-28 17:29:17 +0200

    Added GOE display in ElementPreview + function to reset Preview

--- Scripts/ElementPreview.js
+++ Scripts/ElementPreview.js
@@ -2,10 +2,17 @@ import { RenderName } from "./Table.js";
 import { getElement } from "./CreateElementFromUser.js";
 
 export function ElemPreview() {
-   const elemDiv = document.querySelector(".div-elem-disp")
+   const elemDiv = document.querySelector(".Element-display")
+   const Goespan = document.querySelector(".goe-display")
    const ElemePreselected = getElement()
    if (!ElemePreselected.Element) {
       ElemePreselected.Element = ""
    }
    elemDiv.innerHTML = RenderName(ElemePreselected)
+   Goespan.innerHTML = ElemePreselected.GOE > 0 ? ('+' + ElemePreselected.GOE) : ElemePreselected.GOE
+}
+
+export function ResetPreview() {
+   document.querySelector(".Element-display").innerHTML = "Element"
+   document.querySelector(".goe-display").innerHTML = ""
 }
